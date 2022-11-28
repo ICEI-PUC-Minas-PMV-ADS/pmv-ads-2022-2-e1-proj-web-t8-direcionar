9cc137f5c8153e5d1b9b1fbf61c6202eff970d2a
FabricioMaia <114435981+FabricioMaia@users.noreply.github.com>
2022-11-27 21:34:58 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,335 @@
+var resp_num = [{resp_1:''},{resp_2:''}, {resp_3:''}, {resp_4:''}, {resp_5:''}, 
+                {resp_6:''}, {resp_7:''}, {resp_8:''}, {resp_9:''}, {resp_10:''}, 
+                {resp_11:''}, {resp_12:''}, {resp_13:''}, {resp_14:''}];
+
+var resultado = '';
+
+var resp_a = 0;
+var resp_b = 0;
+var resp_c = 0;
+var resp_d = 0;
+var resp_e = 0;
+var resp_f = 0;
+
+function capturar() {
+    resp_1 = document.querySelector('input[name="pergunta1"]:checked').value;
+    resp_2 = document.querySelector('input[name="pergunta2"]:checked').value;
+    resp_3 = document.querySelector('input[name="pergunta3"]:checked').value;
+    resp_4 = document.querySelector('input[name="pergunta4"]:checked').value;
+    resp_5 = document.querySelector('input[name="pergunta5"]:checked').value;
+    resp_6 = document.querySelector('input[name="pergunta6"]:checked').value;
+    resp_7 = document.querySelector('input[name="pergunta7"]:checked').value;
+    resp_8 = document.querySelector('input[name="pergunta8"]:checked').value;
+    resp_9 = document.querySelector('input[name="pergunta9"]:checked').value;
+    resp_10 = document.querySelector('input[name="pergunta10"]:checked').value;
+    resp_11 = document.querySelector('input[name="pergunta11"]:checked').value;
+    resp_12 = document.querySelector('input[name="pergunta12"]:checked').value;
+    resp_13 = document.querySelector('input[name="pergunta13"]:checked').value;
+    resp_14 = document.querySelector('input[name="pergunta14"]:checked').value;
+
+    for (var cont = 0; cont < 1;)  {
+        
+        if (resp_1 === 'a') {
+            resp_a++;
+        }
+        else if (resp_1 === 'b') {
+            resp_b++;
+        }
+        else if (resp_1 === 'c') {
+            resp_c++;
+        }
+        else if (resp_1 === 'd') {
+            resp_d++;
+        }
+        else if (resp_1 === 'e') {
+            resp_e++;
+        }
+        else if (resp_1 === 'f') {
+            resp_f++;
+        }
+        
+        if (resp_2 === 'a') {
+            resp_a++;
+        }
+        else if (resp_2 === 'b') {
+            resp_b++;
+        }
+        else if (resp_2 === 'c') {
+            resp_c++;
+        }
+        else if (resp_2 === 'd') {
+            resp_d++;
+        }
+        else if (resp_2 === 'e') {
+            resp_e++;
+        }
+        else if (resp_2 === 'f') {
+            resp_f++;
+        }
+
+        if (resp_3 === 'a') {
+            resp_a++;
+        }
+        else if (resp_3 === 'b') {
+            resp_b++;
+        }
+        else if (resp_3 === 'c') {
+            resp_c++;
+        }
+        else if (resp_3 === 'd') {
+            resp_d++;
+        }
+        else if (resp_3 === 'e') {
+            resp_e++;
+        }
+        else if (resp_3 === 'f') {
+            resp_f++;
+        }
+
+        if (resp_4 === 'a') {
+            resp_a++;
+        }
+        else if (resp_4 === 'b') {
+            resp_b++;
+        }
+        else if (resp_4 === 'c') {
+            resp_c++;
+        }
+        else if (resp_4 === 'd') {
+            resp_d++;
+        }
+        else if (resp_4 === 'e') {
+            resp_e++;
+        }
+        else if (resp_4 === 'f') {
+            resp_f++;
+        }
+
+        if (resp_5 === 'a') {
+            resp_a++;
+        }
+        else if (resp_5 === 'b') {
+            resp_b++;
+        }
+        else if (resp_5 === 'c') {
+            resp_c++;
+        }
+        else if (resp_5 === 'd') {
+            resp_d++;
+        }
+        else if (resp_5 === 'e') {
+            resp_e++;
+        }
+        else if (resp_5 === 'f') {
+            resp_f++;
+        }
+
+        if (resp_6 === 'a') {
+            resp_a++;
+        }
+        else if (resp_6 === 'b') {
+            resp_b++;
+        }
+        else if (resp_6 === 'c') {
+            resp_c++;
+        }
+        else if (resp_6 === 'd') {
+            resp_d++;
+        }
+        else if (resp_6 === 'e') {
+            resp_e++;
+        }
+        else if (resp_6 === 'f') {
+            resp_f++;
+        }
+
+        if (resp_7 === 'a') {
+            resp_a++;
+        }
+        else if (resp_7 === 'b') {
+            resp_b++;
+        }
+        else if (resp_7 === 'c') {
+            resp_c++;
+        }
+        else if (resp_7 === 'd') {
+            resp_d++;
+        }
+        else if (resp_7 === 'e') {
+            resp_e++;
+        }
+        else if (resp_7 === 'f') {
+            resp_f++;
+        }
+
+        if (resp_8 === 'a') {
+            resp_a++;
+        }
+        else if (resp_8 === 'b') {
+            resp_b++;
+        }
+        else if (resp_8 === 'c') {
+            resp_c++;
+        }
+        else if (resp_8 === 'd') {
+            resp_d++;
+        }
+        else if (resp_8 === 'e') {
+            resp_e++;
+        }
+        else if (resp_8 === 'f') {
+            resp_f++;
+        }
+
+        if (resp_9 === 'a') {
+            resp_a++;
+        }
+        else if (resp_9 === 'b') {
+            resp_b++;
+        }
+        else if (resp_9 === 'c') {
+            resp_c++;
+        }
+        else if (resp_9 === 'd') {
+            resp_d++;
+        }
+        else if (resp_9 === 'e') {
+            resp_e++;
+        }
+        else if (resp_9 === 'f') {
+            resp_f++;
+        }
+
+        if (resp_10 === 'a') {
+            resp_a++;
+        }
+        else if (resp_10 === 'b') {
+            resp_b++;
+        }
+        else if (resp_10 === 'c') {
+            resp_c++;
+        }
+        else if (resp_10 === 'd') {
+            resp_d++;
+        }
+        else if (resp_10 === 'e') {
+            resp_e++;
+        }
+        else if (resp_10 === 'f') {
+            resp_f++;
+        }
+
+        if (resp_11 === 'a') {
+            resp_a++;
+        }
+        else if (resp_11 === 'b') {
+            resp_b++;
+        }
+        else if (resp_11 === 'c') {
+            resp_c++;
+        }
+        else if (resp_11 === 'd') {
+            resp_d++;
+        }
+        else if (resp_11 === 'e') {
+            resp_e++;
+        }
+        else if (resp_11 === 'f') {
+            resp_f++;
+        }
+
+        if (resp_12 === 'a') {
+            resp_a++;
+        }
+        else if (resp_12 === 'b') {
+            resp_b++;
+        }
+        else if (resp_12 === 'c') {
+            resp_c++;
+        }
+        else if (resp_12 === 'd') {
+            resp_d++;
+        }
+        else if (resp_12 === 'e') {
+            resp_e++;
+        }
+        else if (resp_12 === 'f') {
+            resp_f++;
+        }
+
+        if (resp_13 === 'a') {
+            resp_a++;
+        }
+        else if (resp_13 === 'b') {
+            resp_b++;
+        }
+        else if (resp_13 === 'c') {
+            resp_c++;
+        }
+        else if (resp_13 === 'd') {
+            resp_d++;
+        }
+        else if (resp_13 === 'e') {
+            resp_e++;
+        }
+        else if (resp_13 === 'f') {
+            resp_f++;
+        }
+
+        if (resp_14 === 'a') {
+            resp_a++;
+        }
+        else if (resp_14 === 'b') {
+            resp_b++;
+        }
+        else if (resp_14 === 'c') {
+            resp_c++;
+        }
+        else if (resp_14 === 'd') {
+            resp_d++;
+        }
+        else if (resp_14 === 'e') {
+            resp_e++;
+        }
+        else if (resp_14 === 'f') {
+            resp_f++;
+        }
+
+        cont++;
+
+        if(resp_a > resp_b & resp_a > resp_c & resp_a > resp_d & resp_a > resp_e & resp_a > resp_f) {
+            resultado = "resposta1";
+            console.log (resultado);
+        }
+        else if (resp_b > resp_a & resp_b > resp_c & resp_b > resp_d & resp_b > resp_e & resp_b > resp_f) {
+            resultado = "resposta2";
+            console.log (resultado);
+        }
+        else if (resp_c > resp_a & resp_c > resp_b & resp_c > resp_d & resp_c > resp_e & resp_c > resp_f) {
+            resultado = "resposta3";
+            console.log (resultado);
+        }
+        else if (resp_d > resp_a & resp_d > resp_b & resp_d > resp_c & resp_d > resp_e & resp_d > resp_f) {
+            resultado = "resposta4";
+            console.log (resultado);
+        }
+        else if (resp_e > resp_a & resp_e > resp_b & resp_e > resp_c & resp_e > resp_d & resp_e > resp_f) {
+            resultado = "resposta5";
+            console.log (resultado);
+        }
+        else if (resp_f > resp_a & resp_f > resp_b & resp_f > resp_c & resp_f > resp_d & resp_f > resp_e) {
+            resultado = "resposta6";
+            console.log (resultado);
+        }
+
+        // alert (resultado);
+    };  
+    document.getElementById(resultado).style.display = 'block';
+    document.getElementById('questionario').style.display = 'none';
+    document.getElementById('footer-index').style.display = 'none';
+    document.getElementById('footer-3').style.display = 'block';
+    // alert (resp_a, resp_b, resp_c, resp_d, resp_e, resp_f);
+    // console.log(resp_a, resp_b, resp_c, resp_d, resp_e, resp_f);
+    // document.getElementById('resposta1').style.display = 'block';
+    // document.getElementById('questionario').style.display = 'none';
+}    
